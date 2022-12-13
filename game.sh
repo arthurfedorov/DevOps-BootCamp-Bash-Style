8a9f3eb1e825270a8c309147939b04d9e12b3dca
Arthur Fedorov <fedorov.arthur@gmail.com>
2022-12-13 15:11:13 +0400

    Add few fixes

--- game.sh
+++ game.sh
@@ -17,8 +17,8 @@ while [[ "$ch" -ne 3 ]]; do
       x=0; c=0; p=0
       read -r -p "Enter any number between 0 and 9 : " n
 
-      while [[ $c -eq 0 ]]; do
-        x=11; r=($(shuf -i 0-9 -n 10))
+      while [[ "$c" -eq 0 ]]; do
+        x=11; read -r r < <(shuf -i 0-9 -n 10)
         echo "${r[@]}"
         
         for i in {1..10}; do
@@ -32,21 +32,21 @@ while [[ "$ch" -ne 3 ]]; do
           break
         fi
 
-        if [ ${r[$(($x))-1]} -eq $n ]; then
+        if [ "${r[$(($x))-1]}" -eq "$n" ]; then
           echo "Great"
-          ((p=p+1))
+          ( (p=p+1) )
         else
             c=1
             break
         fi
       done
-    elif [[ $ch -eq 2 ]]; then
+    elif [[ "$ch" -eq 2 ]]; then
       echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
     else
         break
     fi
 
-    if [[ $c -eq 1 ]]; then
+    if [[ "$c" -eq 1 ]]; then
       echo -e "\nGAME OVER\n"
       echo "You scored $p points"
     fi
